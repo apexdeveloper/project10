668847668017eaca7301afb3abe3c8d6c2642b15
apexdeveloper <ramit.singh@ramitsingh-ltm.internal.salesforce.com>
2015-06-02 01:58:33 -0700

    new changes - version 1.1

--- newExpressApp/routes/index.js
+++ newExpressApp/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/', function(req, res) {
+	res.render('index', {
+		title: 'Express App',
+		desc: 'new visualization Application to analyze your application events'
+	});
+
 });
+
 
 module.exports = router;
